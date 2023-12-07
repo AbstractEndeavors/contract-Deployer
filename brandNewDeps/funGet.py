a8ccdcb9e1cd523d1dc4f524d4c2528fab615cbb
putkoff <57512254+putkoff@users.noreply.github.com>
2023-12-07 05:40:07 -0600

    Delete brandNewDeps directory

--- brandNewDeps/funGet.py
+++
@@ -1,8 +0,0 @@
-import contractParse as parse
-import json
-def Add(x):
-		return parse.functionGet(x,js)
-global js
-js = json.loads(str(parse.reader("takeIt.json")).replace("'",'"'))
-js = Add("function updateManagers(address newVal) external onlyOwner ")
-parse.pen(js,"takeIt.json")
